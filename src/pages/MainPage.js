78f9f2d192f4324319193ae2432c545332a2b341
Marco <cwhong8087@gmail.com>
2023-09-17 00:27:39 +0900

    update

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,41 @@
-import Button from '../Components/common/Button';
+import styled from 'styled-components';
 import Header from '../Components/common/Header';
+import img01 from '../image/img01.png';
+import img12 from '../image/img12.png';
+import img06 from '../image/img06.png';
+
+const TotalBox = styled.div`
+  text-align: center;
+  font-size: 1.5rem;
+  padding-bottom: 100px;
+  .img {
+    width: 800px;
+  }
+  .mention {
+    padding: 50px;
+  }
+`;
 
 const MainPage = () => {
   return (
-    <div>
+    <>
       <Header />
-      <Button>버튼</Button>
-    </div>
+      <TotalBox>
+        <div>
+          <div className="mention">저희 호윤상사 주식회사에서는</div>
+          <img className="img" src={img01} alt="img01" />
+          <div className="mention">
+            건설 현장의 새로운 안전 패러다임을 제시합니다
+          </div>
+          <img className="img" src={img06} alt="img06" />
+          <div className="mention">
+            근로자와 사업주 그리고 이용자 모두가 웃을 수 있도록
+          </div>
+          <img className="img" src={img12} alt="img12" />
+          <div className="mention">(주)호윤상사가 함께 하겠습니다.</div>
+        </div>
+      </TotalBox>
+    </>
   );
 };
 export default MainPage;
