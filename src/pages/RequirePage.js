8fb576a93c49723cda8efef5a4242c548128ee23
Marco <cwhong8087@gmail.com>
2023-09-17 01:12:15 +0900

    up

--- src/pages/RequirePage.js
+++ src/pages/RequirePage.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import NormalHeader from '../Components/common/NormalHeader';
 import img07 from '../image/img07.png';
 import img05 from '../image/img05.png';
+import img12 from '../image/img12.png';
 
 const TotalBox = styled.div`
   text-align: center;
@@ -22,8 +23,12 @@ const RequirePage = () => {
       <TotalBox>
         <div className="mention">건설에선 언제나 안전이 최선입니다</div>
         <img className="img" src={img07} alt="img07" />
-        <div className="mention">안전을 위한 완벽한 환경을 조성하는 것</div>
+        <div className="mention">근로자를 위한 완벽한 환경을 조성하는 것</div>
         <img className="img" src={img05} alt="img05" />
+        <div className="mention">
+          그것이 저희 (주)호연안전상사의 마지막 목표입니다
+        </div>
+        <img className="img" src={img12} alt="img12" />
       </TotalBox>
     </>
   );
